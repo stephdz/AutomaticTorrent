368a6b9af8fc65f17428d1f8cfda1693b0752fc6
Stéphane DESHIERE <sdeshiere@yahoo.fr>
2012-06-10 17:09:21 +0200

    Ajout d'un indicateur de progression utile pour nautilus-actions

--- bin/open-subtitles.sh
+++ bin/open-subtitles.sh
@@ -2,7 +2,7 @@
 ##############################################################################################
 # open-subtitles.sh : Téléchargement des sous-titres sur opensubtitles.org
 # Paramètres :
-#  - $1 : options (-v pour plus de logs)
+#  - $1 : options (-v pour plus de logs, -w pour avoir la progression dans un popup)
 #  - $2 : langue (fre pour français)
 #  - $3 : chemin vers la vidéo ou nom de fichier (existance non obligatoire)
 ##############################################################################################
@@ -10,5 +10,39 @@
 # Inclusion des fonctions communes
 . $(dirname $0)/functions.sh
 
+# Recherche des paramètres
+WINDOWED="false"
+OPTIONS=""
+LANG="fre"
+VIDEO=""
+
+# On commence par les options
+for param in $@; do
+	if [ "$param" = "-${param:1}" ]; then
+		if [ "$param" = "-w" ]; then
+			WINDOWED="true"
+		else
+			OPTIONS="$OPTIONS $param"
+		fi
+		shift
+	else
+		break
+	fi
+done
+
+# La langue et le fichier à traiter
+LANG=$1
+VIDEO=$2
+
 # Exécution du Jar avec les paramètres fournis
-java -jar "$OPEN_SUBTITLES_JAR" "$1" "$2" "$3"
+if [ "$WINDOWED" = "true" ]; then
+	java -jar "$OPEN_SUBTITLES_JAR" $OPTIONS "$LANG" "$VIDEO" | sed 's/#/-/g'  | zenity --progress \
+		--title="Recherche de sous-titres" \
+		--text="Recherche en cours..." \
+		--percentage=0 \
+		--pulsate \
+		--auto-close \
+		--no-cancel
+else
+	java -jar "$OPEN_SUBTITLES_JAR" $OPTIONS "$LANG" "$VIDEO"
+fi
