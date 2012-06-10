4af5dad77381b2ddf7ccb80b162959569221a09e
Stéphane DESHIERE <sdeshiere@yahoo.fr>
2012-06-10 13:05:13 +0200

    Corrections mineures

--- bin/open-subtitles.sh
+++ bin/open-subtitles.sh
@@ -2,13 +2,13 @@
 ##############################################################################################
 # open-subtitles.sh : Téléchargement des sous-titres sur opensubtitles.org
 # Paramètres :
-#  - $1 : langue (fre pour français)
-#  - $2 : chemin vers la vidéo ou nom de fichier (existance non obligatoire)
-# Les logs sont présents dans le fichier /var/lib/deluge/logs/todo_watcher.log
+#  - $1 : options (-v pour plus de logs)
+#  - $2 : langue (fre pour français)
+#  - $3 : chemin vers la vidéo ou nom de fichier (existance non obligatoire)
 ##############################################################################################
 
 # Inclusion des fonctions communes
 . $(dirname $0)/functions.sh
 
 # Exécution du Jar avec les paramètres fournis
-java -jar "$OPEN_SUBTITLES_JAR" $@
+java -jar "$OPEN_SUBTITLES_JAR" "$1" "$2" "$3"
